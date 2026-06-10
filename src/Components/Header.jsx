0a48f6fa487059a94f75ace1a58efa19398d0239
goutami-pradhan <goutamipradhan462@gmail.com>
2026-06-10 09:38:31 +0000

    3rd commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,24 @@
 import React from "react";
+import ThemeToggle from "./ThemeToggle";
 
-function Header({ currentPage, setCurrentPage}){
-    return(
-        <header style={styles.header}>
-            <h1 style={styles.logo}> ShopSmart</h1>
-            <nav style={styles.nav}></nav>
-            <button onClick={()=> setCurrentPage("home")}>Home</button>
-            <button onClick={()=> setCurrentPage("about")}>About</button>
-            <button onClick={()=> setCurrentPage("contact")}>Contact</button>
-        </header>
-    );
+function Header({ currentPage, setCurrentPage, theme, setTheme }) {
+  return (
+    <header style={{ 
+      display: "flex", 
+      justifyContent: "space-between", 
+      padding: "10px", 
+      background: theme === "light" ? "#282c34" : "#111", 
+      color: "#fff" 
+    }}>
+      <h1>ShopSmart</h1>
+      <nav style={{ display: "flex", gap: "10px" }}>
+        <button onClick={() => setCurrentPage("home")}>Home</button>
+        <button onClick={() => setCurrentPage("about")}>About</button>
+        <button onClick={() => setCurrentPage("contact")}>Contact</button>
+        <ThemeToggle theme={theme} setTheme={setTheme} />
+      </nav>
+    </header>
+  );
 }
-const styles={
-    header:{display: "flex", justifyContent: "space-between", padding: "10px", background: "#282c34", color:"#fff"},
-    logo: {margin: 0},
-    nav: {display: "flex", gap: "10px"}
-};
 
 export default Header;
